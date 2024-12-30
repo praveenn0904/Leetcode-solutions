9a7a3b5345d3eb3ef96f6757a51f6b973782a0ae
praveenn0904 <149926740+praveenn0904@users.noreply.github.com>
2024-12-30 14:22:24 +0530

    Time: 4 ms (94.81%), Space: 45.5 MB (39.45%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        
+        if (s == null || s.isEmpty() || s.length() < k)
+      return false;
+    
+    int[] count = new int[26];
+    //count freq of every character
+    for (char ch : s.toCharArray()) ++count[ch - 'a'];
+    //count chars that has odd freq
+    int countOfOdd = 0;
+    for (int i = 0; i < 26; i++) 
+      if (count[i] % 2 == 1)
+        ++countOfOdd;
+    return countOfOdd <= k;
+    }
+}
